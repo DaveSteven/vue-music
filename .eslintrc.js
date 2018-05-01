b07faf4f7f517eeaeeb635e244a7a810f2af44fa
david <david.han.jn@outlook.com>
2018-05-01 11:06:01 +0800

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,22 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential'],
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
   // add your custom rules here
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    'eol-last': 0,
+    'quotes': ['error', 'single'],
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'space-before-function-paren': 0
   }
 }
